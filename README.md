60fadb0aac6d86cdc54a93259d8186b7b4cdcc42
Ryo Ota <nwtgck@gmail.com>
2018-02-16 08:44:02 +0900

    [add] Add a practice for open-union

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # open-union-prac
+
+## Run
+
+```hs
+stack build && stack exec open-union-prac-exe
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+-- (from: https://github.com/bfops/open-union)
+
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
--- TODO: Implement
+import Data.OpenUnion
+
+showMyUnion :: Union '[Char, Int, [()]] -> String
+showMyUnion
+    =  (\(c :: Char) -> "char: " ++ show c)
+    @> (\(i :: Int) -> "int: " ++ show i)
+    @> (\(l :: [()]) -> "list length: " ++ show (length l))
+    @> (\(s :: String) -> "string: " ++ s)
+    @> typesExhausted
+
 main :: IO ()
 main = do
-  putStrLn "hello, world from stack executable template"
+    putStrLn $ showMyUnion $ liftUnion (4 :: Int)
+    putStrLn $ showMyUnion $ liftUnion 'a'
+    putStrLn $ showMyUnion $ liftUnion [(), ()]
